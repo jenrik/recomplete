5e7f15f3e4b864ca691cf70d8e5c760228e46f34
jenrik <jacob@jener.dk>
2014-10-13 17:15:56 +0200

    First working version

---
+++ README.md
@@ -0,0 +1,9 @@
+Recomplete
+==========
+Recomplete is a twitter bot that retweet people who tweet about there redirection campaign completions.
+The bot can be found on [@recomplete](https://twitter.com/recomplete) on twitter
+
+License
+=======
+Recomplete is licensed under the GNU General Public license version 3. 
+A copy of the license can be found in the LICENSE.txt file.
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (recomplete) is running..');
-}).listen(9889);
+var util = require("util");
+var twitter = require("twitter");
+var twit = new twitter({
+	"consumer_key": process.env["consumerKey"],
+	"consumer_secret": process.env["consumerSecret"],
+	"access_token_key": process.env["accessTokenKey"],
+	"access_token_secret": process.env["accessTokenSecret"]
+});
+
+var regex = "I completed the campaign \".*?\" by @\\S*? in @redirectiongame";
+
+twit.stream("user", { track: "@redirectiongame" }, function(stream) {
+	stream.on("data", function(data) {
+		if (data.text !== undefined) {
+			if (data.text.match(regex) !== null) {
+				twit.post("/statuses/retweet/" + data["id_str"] + ".json", function() {});
+			}
+		}
+	});
+});
